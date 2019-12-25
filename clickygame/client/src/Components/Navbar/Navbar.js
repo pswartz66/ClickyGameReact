9b24ab8d1f386fd5031c024eaecd192c97279f39
Phil Swartz <pswartz.sju@gmail.com>
2019-12-25 10:06:12 -0500

    moved files, added sample navbar to test react was working

---
+++ clickygame/client/src/Components/Navbar/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+
+const Navbar = props => {
+    return (
+
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+
+            <ul className="navbar-nav">
+                <li className="nav-item">Clicky Game</li>
+            </ul>
+
+        </nav>
+
+    )
+}
+
+export default Navbar;
+
