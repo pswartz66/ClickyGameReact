5f751b161f00bec89f9c102d8cf3d9ffd3463a0f
Phil Swartz <pswartz.sju@gmail.com>
2019-12-26 10:42:53 -0500

    added google font theme and centered header columns

--- clickygame/client/src/Components/Navbar/Navbar.js
+++ clickygame/client/src/Components/Navbar/Navbar.js
@@ -1,15 +1,24 @@
 import React from 'react';
-
+import './Navbar.css';
 
 
 const Navbar = props => {
     return (
 
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark nav-main">
 
             <ul className="navbar-nav">
                 <li className="nav-item">Clicky Game</li>
             </ul>
+
+            <ul className="navbar-nav">
+                <li className="nav-item">Click and image to begin!</li>
+            </ul>
+
+            <ul className="navbar-nav">
+                <li className="nav-item">Score: 0 | Top Score: 0</li>
+            </ul>
+
 
         </nav>
 
