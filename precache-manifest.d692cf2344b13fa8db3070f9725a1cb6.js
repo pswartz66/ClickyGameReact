01db3c732fcb6e13868f28afdb2279f7f74c595c
Phil Swartz <pswartz.sju@gmail.com>
2019-12-31 13:09:48 -0500

    Updates

---
+++ precache-manifest.d692cf2344b13fa8db3070f9725a1cb6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "148fdc9dc40abcbc7d76fb3bd0eae787",
+    "url": "/ClickyGameReact/index.html"
+  },
+  {
+    "revision": "a4676f0e5356ced3174c",
+    "url": "/ClickyGameReact/static/css/main.37379b01.chunk.css"
+  },
+  {
+    "revision": "3c075fa0eb90d185d7e6",
+    "url": "/ClickyGameReact/static/js/2.5abfb3b8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ClickyGameReact/static/js/2.5abfb3b8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a4676f0e5356ced3174c",
+    "url": "/ClickyGameReact/static/js/main.d82fb9e9.chunk.js"
+  },
+  {
+    "revision": "021d400b47a7315218af",
+    "url": "/ClickyGameReact/static/js/runtime-main.12a6fe11.js"
+  }
+]);
